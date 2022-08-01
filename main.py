a9b8e945cc616083993b15c4cd9c60c7ac813630
benimustiko <beni.mustiko.a@gmail.com>
2022-08-01 23:52:54 +0700

    menambah kan function sort data dan start chrome driver

---
+++ main.py
@@ -0,0 +1,14 @@
+def sortDataListOfDict(self, data, key):
+    result = [dict(t) for t in {tuple(d.items()) for d in data}]
+    result = sorted(result, key=lambda x: x[key], reverse=True)
+    return result
+
+
+def startChromeDriver(self, lokasi_driver):
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-data-dir=D:\\MASTER\\userdata")
+    # options.add_argument('--headless')
+    driver = webdriver.Chrome(
+        executable_path=lokasi_driver, options=options)
+    driver.maximize_window()
+    return driver
