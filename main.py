37ec626a3cb9ee8d063583c0600b097f39cfec30
benimustiko <beni.mustiko.a@gmail.com>
2022-08-01 23:55:06 +0700

    menambah kan komentar pada function sort data dan start chrome driver

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# menyortir data dictionary di dalam list berdasarkan key tertentu
+
+
 def sortDataListOfDict(self, data, key):
     result = [dict(t) for t in {tuple(d.items()) for d in data}]
     result = sorted(result, key=lambda x: x[key], reverse=True)
     return result
+
+# inisialisasi chrome driver selenium
 
 
 def startChromeDriver(self, lokasi_driver):
